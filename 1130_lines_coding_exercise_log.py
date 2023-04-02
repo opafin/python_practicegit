999890f72b3cf5d9c879266549a346913ab08772
Olli Lantela <122178992+opafin@users.noreply.github.com>
2023-04-02 20:42:43 +0300

    Update 1130_lines_coding_exercise_log.py

--- 1130_lines_coding_exercise_log.py
+++ 1130_lines_coding_exercise_log.py
@@ -5,6 +5,8 @@ import os
 import inspect
 import math
 
+# Hello! this is an exercise log to show effort at a glance.
+# For effect, have a scroll through faster than the eye can see  :)
 
 def main():
 
@@ -44,9 +46,9 @@ def main():
 
 
 def bury_the_treasure():
-    row1 = ["⬜️", "️⬜️", "️⬜️"]
-    row2 = ["⬜️", "⬜️", "️⬜️"]
-    row3 = ["⬜️️", "⬜️️", "⬜️️"]
+    row1 = ["", "️", ""]
+    row2 = ["", "", "️"]
+    row3 = ["", "", ""]
     map = [row1, row2, row3]
     print(f"{row1}\n{row2}\n{row3}")
     position = input("Where do you want to put the treasure? ")
